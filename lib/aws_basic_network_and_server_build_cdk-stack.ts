d05fdc30f4e6af49180376f4a11a28f6b3b85369
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-05-06 14:42:53 +0900

    make vpc

--- lib/aws_basic_network_and_server_build_cdk-stack.ts
+++ lib/aws_basic_network_and_server_build_cdk-stack.ts
@@ -1,19 +1,33 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+// ec2 に関するパッケージを import
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class AwsBasicNetworkAndServerBuildCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsBasicNetworkAndServerBuildCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // VPC の作成
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      vpcName: 'VPC',
+      maxAzs: 1,
+      createInternetGateway: true,
+      natGateways: 1,
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+
+      // サブネットの設定
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'パブリックサブネット',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'プライベートサブネット',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
     });
-
-    const topic = new sns.Topic(this, 'AwsBasicNetworkAndServerBuildCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
