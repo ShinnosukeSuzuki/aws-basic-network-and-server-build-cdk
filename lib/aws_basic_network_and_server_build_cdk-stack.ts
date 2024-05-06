14b2e743910dafe93ac47f15c691cf98e21ff742
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-05-06 15:29:26 +0900

    Web Serverのインスタンスの作成

--- lib/aws_basic_network_and_server_build_cdk-stack.ts
+++ lib/aws_basic_network_and_server_build_cdk-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 // ec2 に関するパッケージを import
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
@@ -28,6 +28,30 @@ export class AwsBasicNetworkAndServerBuildCdkStack extends Stack {
           subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
         },
       ],
+    });
+
+    // EC2 インスタンス(Web Server)の作成
+    // Web Serverのセキュリティグループを作成
+    const webServerSecurityGroup = new ec2.SecurityGroup(this, 'WebServerSecurityGroup', {
+      vpc: vpc,
+      securityGroupName: 'WEB-SG',
+    });
+
+    // Web Serverのセキュリティグループにインバウンドルールを追加
+    webServerSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic from anywhere');
+    webServerSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH traffic from anywhere');
+
+    // Web Serverのインスタンスを作成
+    const webServer = new ec2.Instance(this, 'WebServer', {
+      instanceName: 'WEBサーバー',
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023 }),
+      vpc: vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroup: webServerSecurityGroup,
+      keyPair: ec2.KeyPair.fromKeyPairName(this, 'KeyPair', 'my-key'),
     });
   }
 }
